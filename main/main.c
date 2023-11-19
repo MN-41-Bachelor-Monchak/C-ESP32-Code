c38ace48c5651bd23d44623342b49f2cc0b63d5a
ColderHost <volodymyr.monchak@redtag.pro>
2023-11-19 17:56:13 +0200

    Update

--- main/main.c
+++ main/main.c
@@ -1,4 +1,15 @@
+// Libraries import
 #include <stdio.h>
+#include "MAX30102/max30102.h"
+
+/*
+How to connect MAX30102 (Violet):
+    3v3 -> VIN
+    GND -> GND
+    P22 -> SCL
+    P21 -> SDA
+*/
+
 
 void app_main(void) {
 
